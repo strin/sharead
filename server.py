d7fcc959cb319a770ee0ff228373f1b087326c02
Tianlin Shi <tianlins@stanford.edu>
2015-09-11 14:48:30 +0800

    server side changes to support upload

--- server.py
+++ server.py
@@ -1,3 +1,45 @@
-import shareread.storage.dropbox as store
+#import shareread.storage.dropbox as store
+import shareread.storage.local as store
+from shareread.server.utils import parse_webkitform
+from tornado import (ioloop, web)
+import md5
+import base64
+from StringIO import StringIO
+
+class UploadHandler(web.RequestHandler):
+    def get(self):
+        self.render("upload.html")
+
+class UploadSubmitHandler(web.RequestHandler):
+    def post(self):
+        form = parse_webkitform(self.request.body)
+        # get data.
+        data = form['data']
+        data_stream = StringIO(data)
+        # get filehash.
+        md5_code = md5.new()
+        md5_code.update(data)
+        filehash = base64.b64encode(md5_code.digest())
+        store.put_file(store.TEST_ACCESS_TOKEN, filehash, data_stream)
+
+handlers = [
+    (r"/img/(.*)", web.StaticFileHandler, {"path": "frontend/static/img/"}),
+    (r"/css/(.*)", web.StaticFileHandler, {"path": "frontend/static/css/"}),
+    (r"/js/(.*)", web.StaticFileHandler, {"path": "frontend/static/js/"}),
+    (r"/upload", UploadHandler),
+    (r"/upload-submit", UploadSubmitHandler)
+]
+
+settings = {
+    "autoreload": True,
+    "debug": True,
+    "template_path": "frontend/template/"
+}
+
+
+if __name__ == "__main__":
+    application = web.Application(handlers, **settings)
+    application.listen(8888, address="0.0.0.0")
+    ioloop.IOLoop.current().start()
 
 
--- shareread/storage/dropbox.py
+++ shareread/storage/dropbox.py
@@ -1,14 +1,15 @@
 import external.dropbox as dbx
 
 APP_KEY   = '1gtl0f8cj9tj6j9'
-APP_TOKEN = '5osbesiib74rgyh'
+APP_SECRET = 'dshgb079kgt8qmr'
+TEST_ACCESS_TOKEN = 'F32ygv2k14cAAAAAAAFY9rqPb-xtlAh9wzRJZSLfQ-brGFCDAa5rKZdzpywb6Td1' #TODO: remove.
 
 def _get_access_token():
     """
     Returns a url using which users can log in Dropbox and get access token
     for us.
     """
-    flow = dbx.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_TOKEN)
+    flow = dbx.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
     authorize_url = flow.start()
     return authorize_url
 
@@ -16,15 +17,24 @@ def put_file(access_token, path, stream):
     """
     Use the access_token to put the file in Dropbox path.
     """
-    with dbx.client.DropboxClient(access_token) as client:
-        response = client.put_file(paht, stream)
-        return response #(TODO) wrap the response.
+    print '<<<<< put file >>>>>>'
+    client = dbx.client.DropboxClient(access_token)
+    response = client.put_file(path, stream)
+    return response #(TODO) wrap the response.
 
 def get_file(access_token, path):
     """
     Use the access_token to get file from path.
     Return a stream.
     """
-    with dbx.client.DropboxClient(access_token) as client:
-        stream, metadata = client.get_file_and_metadata(path)
-        return stream
+    client = dbx.client.DropboxClient(access_token)
+    stream, metadata = client.get_file_and_metadata(path)
+    return stream
+
+def account_info(access_token):
+    client = dbx.client.DropboxClient(access_token)
+    return client.account_info()
+
+
+if __name__ == '__main__':
+    print 'authorize_url', _get_access_token()
