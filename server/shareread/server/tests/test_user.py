114ae13029905c2f39bca912e04b652f01590b96
Tianlin Shi <tianlins@stanford.edu>
2016-06-10 15:20:19 -0700

    fix missing os

--- server/shareread/server/tests/test_user.py
+++ server/shareread/server/tests/test_user.py
@@ -1,9 +1,15 @@
 from shareread.server.user import create_user_from_google, authorize_google, userid_by_googleid
 import shareread.server.db as db
+import os
+
+
+def cleanup_db():
+    if os.path.exists(db.DB_FILE_NAME):
+        os.remove(db.DB_FILE_NAME)
+
 
 def test_create_user():
-    if os.path.exists(DB_FILE_NAME):
-        os.remove(DB_FILE_NAME)
+    cleanup_db()
     userid = create_user_from_google(googleid='1', name='Tim',
             email='tim@sharead.org',
             access_token='xxxxx'
