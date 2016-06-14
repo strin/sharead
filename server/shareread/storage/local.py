d42203db7d2502555960e6741a00bbdec3ab7869
Tianlin Shi <tianlins@stanford.edu>
2016-06-14 14:44:53 -0700

    add get_url for local storage too

--- server/shareread/storage/local.py
+++ server/shareread/storage/local.py
@@ -4,21 +4,24 @@ import os
 from datetime import datetime
 from shareread.utils import mkdir_if_necessary
 
-TEST_ACCESS_TOKEN = 'strin'
+ACCESS_TOKEN = 'strin'
 
 def _get_access_token():
     return str(datetime.now())
 
-def get_local_path(access_token, path):
-    return os.path.join('upload', access_token, path)
+def get_local_path(path):
+    return os.path.join('upload', ACCESS_TOKEN, path)
 
-def put_file(access_token, path, stream):
-    local_path = get_local_path(access_token, path)
+def put_file(path, stream):
+    local_path = get_local_path(ACCESS_TOKEN, path)
     mkdir_if_necessary(os.path.dirname(local_path))
     with open(local_path, 'w') as f:
         f.write(stream.read())
 
-def get_file(access_token, path):
-    local_path = get_local_path(access_token, path)
+def get_file(path):
+    local_path = get_local_path(ACCESS_TOKEN, path)
     return open(local_path, 'r')
+
+def get_url(path):
+    return get_local_path(path)
 
